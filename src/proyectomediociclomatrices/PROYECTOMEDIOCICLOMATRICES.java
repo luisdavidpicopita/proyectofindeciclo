b7df7883f097f197ede5e8a7e6c4352fb5679883
pik0h <pik0h@DESKTOP-NB2I8DG>
2024-01-19 16:26:04 -0500

    Segundo commit

--- src/proyectomediociclomatrices/PROYECTOMEDIOCICLOMATRICES.java
+++ src/proyectomediociclomatrices/PROYECTOMEDIOCICLOMATRICES.java
@@ -1,51 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectomediociclomatrices;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
-/**
- *
- * @author pik0h
- */
 public class PROYECTOMEDIOCICLOMATRICES {
 
-    /**
-     * @param args the command line arguments
-     */
+    //AUTOR: PICO PITA LUIS DAVID
     public static void main(String[] args) {
-        //ESCANER
         Scanner sc = new Scanner(System.in);
-
-        //VARIABLE DE MATRIZ MxM
-        int dimensión;
-
-        //PROGRAMA DE MATRIZ MxM
-        int op;
+        boolean error = true;
         do {
-            System.out.println("\n===================MENÚ===================");
-            System.out.println("1. Imprimir matrices");
-            System.out.println("0. Salir");
-            System.out.print("Ingrese una opción: ");
-            op = sc.nextInt();
-            System.out.println("==========================================");
-            switch (op) {
-                case 1:
-                    System.out.print("###Ingrese las dimensiones de la matriz MxM: ");
-                    dimensión = sc.nextInt();
-                    String m[][] = new String[dimensión][dimensión];
-                    Matriz matriz = new Matriz();
-                    matriz.insertar(m, dimensión);
-                    System.out.println("\n");
-                    matriz.mostrar_a(m, dimensión);
-                    System.out.println("\n");
-                    matriz.mostrar_b(m, dimensión);
-                    break;
+            try {
+                int op;
+                do {
+                    System.out.println("=======================MENÚ=======================");
+                    System.out.println("1. Imprimir matrices");
+                    System.out.println("0. Salir");
+                    System.out.print("Ingrese una opción: ");
+                    op = sc.nextInt();
+                    System.out.println("==================================================");
+                    switch (op) {
+                        case 1:
+                            System.out.print("###Ingrese las dimensiones de la matriz MxM: ");
+                            try {
+                                int dimensión = sc.nextInt();
+                                String m[][] = new String[dimensión][dimensión];
+                                Matriz matriz = new Matriz();
+                                matriz.insertar(m, dimensión);
+                                matriz.mostrar_a(m, dimensión);
+                                matriz.mostrar_b(m, dimensión);
+                            } catch (NegativeArraySizeException n) {
+                                System.out.println("==================================================");
+                                System.out.println("¡¡¡INGRESE UN NÚMERO ENTERO POSITIVO!!!");
+                            }
+                            break;
+                        case 0:
+                            System.out.println("¡¡¡SE SALIÓ DEL PROGRAMA!!!");
+                            break;
+                        default:
+                            System.out.println("¡¡¡INGRESE 1 o 0!!!");
+                            break;
+                    }
+                } while (op != 0);
+                break;
+            } catch (InputMismatchException ex) {
+                System.out.println("==================================================");
+                System.out.println("¡¡¡INGRESE UN NÚMERO ENTERO!!!");
+                sc.next();
             }
-        } while (op != 0);
-        System.out.println("¡¡¡SE SALIÓ DEL PROGRAMA!!!");
+        } while (error);
     }
-
 }
